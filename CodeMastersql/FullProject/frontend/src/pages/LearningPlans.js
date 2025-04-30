00d41b295b99dd88b3d448b5a76422f86590f9ae
Vidumini16 <viduminipavithra@gmail.com>
2025-04-30 08:54:25 +0530

    fix errors

--- CodeMastersql/FullProject/frontend/src/pages/LearningPlans.js
+++ CodeMastersql/FullProject/frontend/src/pages/LearningPlans.js
@@ -17,8 +17,8 @@ import {
 } from '@mui/material';
 import Leftsidebar from "../components/homepage/Leftsidebar";
 import MenuBookIcon from '@mui/icons-material/MenuBook';
-// import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-// import LinkIcon from '@mui/icons-material/Link';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import LinkIcon from '@mui/icons-material/Link';
 import PersonIcon from '@mui/icons-material/Person';
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 
